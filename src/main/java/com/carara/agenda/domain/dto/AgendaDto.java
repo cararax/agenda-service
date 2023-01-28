1beb0f1c499fa5f4cbd3ad133fc6766d38404a5c
Pedro Carara <cararax@gmail.com>
2023-01-28 12:15:41 -0300

    feat: add validation to AgendaDto

--- src/main/java/com/carara/agenda/domain/dto/AgendaDto.java
+++ src/main/java/com/carara/agenda/domain/dto/AgendaDto.java
@@ -1,15 +1,21 @@
 package com.carara.agenda.domain.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class AgendaDto {
-    private final String description;
+    @NotBlank(message = "Description is mandatory")
+    @Size(max = 100, message = "Description must have a maximum of 100 characters")
+    private String description;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
+    @Future(message = "End date must be in a future time")
     private LocalDateTime endDate;
 
     public AgendaDto(String description, LocalDateTime endDate) {
