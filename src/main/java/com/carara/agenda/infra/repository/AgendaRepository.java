cac499c929a7b5ca75c91c7a46a02ca038f0228e
Pedro Carara <cararax@gmail.com>
2023-02-01 12:21:16 -0300

    feat: add scheduler service that send message to result service to calculate result and set agenda.resultCalculated to true

--- src/main/java/com/carara/agenda/infra/repository/AgendaRepository.java
+++ src/main/java/com/carara/agenda/infra/repository/AgendaRepository.java
@@ -3,10 +3,19 @@ package com.carara.agenda.infra.repository;
 import com.carara.agenda.domain.entity.Agenda;
 import com.carara.agenda.domain.projection.AgendaId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface AgendaRepository extends JpaRepository<Agenda, Long> {
     List<AgendaId> findByResultCalculatedFalseAndEndDateBetween(LocalDateTime endDateStart, LocalDateTime endDateEnd);
+
+    @Transactional
+    @Modifying
+    @Query("update Agenda a set a.resultCalculated = ?1 where a.id = ?2")
+    void updateResultCalculatedById(boolean resultCalculated, Long id);
+
 }
--- src/main/java/com/carara/agenda/service/SchedulerService.java
+++ src/main/java/com/carara/agenda/service/SchedulerService.java
@@ -1,7 +1,10 @@
 package com.carara.agenda.service;
 
 import com.carara.agenda.domain.projection.AgendaId;
+import com.carara.agenda.infra.message.ResultListener;
+import com.carara.agenda.infra.message.response.Result;
 import com.carara.agenda.infra.repository.AgendaRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -14,13 +17,23 @@ import java.util.List;
 public class SchedulerService {
 
     AgendaRepository agendaRepository;
+    ResultListener resultListener;
 
     @Scheduled(cron = "1 * * * * *")
     public void scheduledMethod() {
         List<AgendaId> pastAgendaIdsWithoutResult = agendaRepository
                 .findByResultCalculatedFalseAndEndDateBetween(LocalDateTime.now().minusDays(1), LocalDateTime.now());
-        //todo: send pastAgendaIdsWithoutResult to a queue for processing result
-        //todo: update agendas.resultCalculated to true after processing result
+
         System.out.println("pastAgendaIdsWithoutResult = " + pastAgendaIdsWithoutResult);
+        pastAgendaIdsWithoutResult.forEach(agendaId -> {
+            try {
+                Result result = resultListener.listen(agendaId.getId());
+                if (result != null) {
+                    agendaRepository.updateResultCalculatedById(true, agendaId.getId());
+                }
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException("Error processing result");
+            }
+        });
     }
 }
