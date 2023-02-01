9f1134e61b57edc1101f670fa525237c4e908939
Pedro Carara <cararax@gmail.com>
2023-02-01 12:08:27 -0300

    feat: add error handlind on message exchanges

--- src/main/java/com/carara/agenda/infra/message/ResultListener.java
+++ src/main/java/com/carara/agenda/infra/message/ResultListener.java
@@ -4,6 +4,7 @@ import com.carara.agenda.infra.message.response.Result;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpIllegalStateException;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,11 +26,15 @@ public class ResultListener {
     ObjectMapper newObjectMapper = new ObjectMapper();
 
     public Result listen(Long agendaId) throws JsonProcessingException {
-        log.info(" [1] Requesting result for " + agendaId);
+        log.info(" [1] Requesting result for agenda " + agendaId);
+        String response = (String) template.convertSendAndReceive(resultExchange.getName(), resulRoutingKey, agendaId);
 
-        String reponse = (String) template.convertSendAndReceive(resultExchange.getName(), resulRoutingKey, agendaId);
-        Result result= newObjectMapper.readValue(reponse, Result.class);
-        log.info(" [4] Returned ");
+        if (response == null) {
+            throw new AmqpIllegalStateException("Impossible to calculate result, try again later.");
+        }
+
+        Result result= newObjectMapper.readValue(response, Result.class);
+        log.info(" [8] Returned , data: " + result.toString());
         return result;
     }
 }
--- src/main/java/com/carara/agenda/infra/message/config/RabbitMQConfig.java
+++ src/main/java/com/carara/agenda/infra/message/config/RabbitMQConfig.java
@@ -3,9 +3,15 @@ package com.carara.agenda.infra.message.config;
 
 import com.carara.agenda.infra.message.ResultListener;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler;
+import org.springframework.amqp.rabbit.listener.FatalExceptionStrategy;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.ErrorHandler;
 
 @Configuration
 public class RabbitMQConfig {
@@ -21,5 +27,26 @@ public class RabbitMQConfig {
     @Bean
     public ResultListener client() {
         return new ResultListener();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setErrorHandler(errorHandler());
+        return factory;
+    }
+
+    @Bean
+    public ErrorHandler errorHandler() {
+        return new ConditionalRejectingErrorHandler(customExceptionStrategy());
+    }
+
+    @Bean
+    FatalExceptionStrategy customExceptionStrategy() {
+        return new CustomFatalExceptionStrategy();
     }
 }
